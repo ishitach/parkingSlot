1122070fcad6785fbd784535a421a027890c19e3
Ishita Chourasia <ishita.chourasia@olacabs.com>
2021-03-14 21:39:02 +0530

    Create IParkingStratergy.java

---
+++ src/main/java/stratergy/IParkingStratergy.java
@@ -0,0 +1,14 @@
+package stratergy;
+
+import models.ParkingSlot;
+
+import java.util.List;
+
+/**
+ * Created by ishita.chourasia on 14/03/21.
+ */
+public interface IParkingStratergy {
+
+    ParkingSlot add(List<ParkingSlot> slots);
+
+}
