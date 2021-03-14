b1a02e3f4e31db1dee9a2ee9f8adaabf03cb9420
Ishita Chourasia <ishita.chourasia@olacabs.com>
2021-03-14 23:38:15 +0530

    Update DatabaseService.java

--- src/main/java/dao/DatabaseService.java
+++ src/main/java/dao/DatabaseService.java
@@ -1,22 +1,49 @@
 package dao;
 
+import models.ParkingFloor;
+import models.ParkingSlot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by ishita.chourasia on 07/03/21.
  */
 public class DatabaseService {
 
-
+    private Map<Integer, ParkingFloor> floorMap;
+    private int floors;
     private static DatabaseService databaseService;
 
-    private DatabaseService(){
+    private DatabaseService(int floors){
+        floorMap = new HashMap<Integer, ParkingFloor>();
+        this.floors = floors;
+        for(int i=0;i<floors;i++) {
+            ParkingFloor parkingFloor = new ParkingFloor();
+            List<ParkingSlot> slotsList = new ArrayList<ParkingSlot>();
+            for (int j = 1; j <= 20; j++) {
+                ParkingSlot slot = new ParkingSlot();
+                slot.setSlotNo(j);
+                slotsList.add(slot);
+            }
+            parkingFloor.setSlots(slotsList);
+            parkingFloor.setFloorNumber(i + 1);
+            floorMap.put(i + 1, parkingFloor);
+        }
 
     }
 
-    public static DatabaseService getInstance(){
+    public static DatabaseService getInstance(int floors){
 
         if(databaseService == null){
-            databaseService = new DatabaseService();
+            databaseService = new DatabaseService(floors);
         }
         return databaseService;
+    }
+
+    public Map<Integer, ParkingFloor> getFloorMap(){
+        return floorMap;
     }
 }
