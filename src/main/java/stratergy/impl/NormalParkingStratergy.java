15b0356008b2b2117a1cfd2cd0cc81af2e253405
Ishita Chourasia <ishita.chourasia@olacabs.com>
2021-03-14 21:39:04 +0530

    Create NormalParkingStratergy.java

---
+++ src/main/java/stratergy/impl/NormalParkingStratergy.java
@@ -0,0 +1,29 @@
+package stratergy.impl;
+
+import models.ParkingSlot;
+import models.Ticket;
+import stratergy.IParkingStratergy;
+
+import java.util.List;
+
+/**
+ * Created by ishita.chourasia on 14/03/21.
+ */
+public class NormalParkingStratergy implements IParkingStratergy {
+
+    public ParkingSlot add(List<ParkingSlot> slots) {
+
+        for (int slot=0;slot<2;slot++) {
+            ParkingSlot currentSlot = slots.get(slot);
+            if (!currentSlot.isBottomFull()) {
+                currentSlot.setBottomFull(true);
+                return currentSlot;
+            } else if (!currentSlot.isTopFull()) {
+                currentSlot.setTopFull(true);
+                return currentSlot;
+            }
+        }
+        return null;
+    }
+
+}
